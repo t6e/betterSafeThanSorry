1a3778a959b4aaafbcfc5b2c01c2babeb352b937
t6e <m4chinehe4rt@gmail.com>
2021-05-22 21:12:38 +0900

    Create database 'sqlite'

---
+++ database/migrations/2021_05_22_103824_create_stock_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStockTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock', function (Blueprint $table) {
+            $table->bigIncrements('stock_id');
+            $table->unsignedBigInteger('product_id');
+            $table->dateTime('expiration_date');
+            $table->integer('num');
+            $table->foreign('product_id')->references('product_id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock');
+    }
+}
