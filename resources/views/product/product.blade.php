ea58850262a611c19790d96d7be013c763bd281c
t6e <m4chinehe4rt@gmail.com>
2021-05-26 15:52:53 +0900

    Create view when stock 0

--- resources/views/product/product.blade.php
+++ resources/views/product/product.blade.php
@@ -2,27 +2,44 @@
 @section('page_title', $product->product_name)
 @section('content')
     <p>{{ $product->product_info }}</p>
-    <p>賞味期限</p>
-    <?php
+    @if (isset($stock))
+        @php
+            $count = 0;
+            foreach ($stock as $info) {
+                $count = $count + $info->num;
+            }
+        @endphp
+        @if ($count == 0)
+            <p>現在、在庫はありません</p>
+        @else
+            <p>賞味期限</p>
+        @endif
+    @else
+        <p>現在、在庫はありません</p>
+    @endif
+    @php
     use App\Models\{Product, Stock};
     $added = false;
     if (isset($cart)) {
-    $stock_id_list = array_column($cart, 'stock_id');
+        $stock_id_list = array_column($cart, 'stock_id');
     }
-    ?>
+    @endphp
     <form action="/cart/add" method="POST" id="cart_form">
         @csrf
         <input name="product_id" type="hidden" value={{ $product->product_id }}>
         @foreach ($stock as $info)
-            <?php
-            $added = false;
-            if (isset($stock_id_list)) {
-            $added = in_array($info->stock_id, $stock_id_list);
-            }
-            ?>
-            <input name="stock_id" type="radio" onclick="howMany({{ $info->num }},{{ $added }})"
-                value={{ $info->stock_id }}>{{ $info->expiration_date }}
+            @php
+                $added = false;
+                if (isset($stock_id_list)) {
+                    $added = in_array($info->stock_id, $stock_id_list);
+                }
+            @endphp
+            @if ($info->num > 0)
+                <input name="stock_id" type="radio" onclick="howMany({{ $info->num }},{{ $added }})"
+                    value={{ $info->stock_id }}>{{ $info->expiration_date }}
+            @endif
         @endforeach
+
         <div id="product_num"></div>
     </form>
 @endsection
